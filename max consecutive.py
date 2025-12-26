5e72e6120cb861e8987090aeec47e3ab6d82955c
Nandini <reddynandu356@gmail.com>
2025-12-26 15:50:12 +0530

    Add files via upload

---
+++ max consecutive.py
@@ -0,0 +1,14 @@
+s=[1,1,1,0,0,1,1,0,1]
+count=0
+res=0
+for num in s:
+  if num==1:
+    count+=1
+  else:
+     if count>res:
+        res=count
+        count=0
+if count>res:
+  res=count
+print(res)
+                                                
