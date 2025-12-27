29dafda981ac30e7e19c82a2aa03995c46c42fc0
Nandini <reddynandu356@gmail.com>
2025-12-27 14:21:58 +0530

    Add files via upload

---
+++ string is a palindrome.py
@@ -0,0 +1,11 @@
+s= "madam"
+def check_palindrome(s):
+    i=0
+    j=len(s)-1
+    while i<j:
+        if s[i]!=s[j]:
+            return False
+        i+=1
+        j-=1
+    return True
+print(check_palindrome(s))
